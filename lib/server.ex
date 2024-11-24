020e644cab3a9dad25882c1045ac1ae293c08625
kriswind <126849955+kriswind@users.noreply.github.com>
2024-11-24 16:06:39 +0200

    codecrafters submit [skip ci]

--- lib/server.ex
+++ lib/server.ex
@@ -16,8 +16,24 @@ defmodule Server do
     # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    {:ok, client} = :gen_tcp.accept(socket)
 
-    :gen_tcp.send(client, "+PONG\r\n")
+    accept_client(socket)
   end
+
+  defp accept_client(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+    accept_client(socket)
+  end
+
+  defp serve(client) do
+    msg = :gen_tcp.recv(client, 0)
+    case msg do
+      {:ok, _} -> :gen_tcp.send(client, "+PONG\r\n")
+      {:error, _} -> :gen_tcp.close(client)
+    end
+
+    serve(client)
+  end
+
 end
